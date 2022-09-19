1b157047d29196954bce95e4c897c9aea7444dd4
Leo Yeung <37359138+leokyeung@users.noreply.github.com>
2022-09-19 15:56:05 -0700

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,4 +13,17 @@
     hamburger.addEventListener("click", toggleMobileNav);
     navList.addEventListener("click", toggleMobileNav);
 
+    $(function(){
+        $(window).scroll(function(){
+            if ($(this).scrollTop() < 50 ) {
+                $("nav-container").removeClass("leo-top-nav")
+            } else {
+                $("nav-container").addClass("leo-top-nav")
+            }
+        })
+    });
+    
+
 })();
+
+//Show / Hide Nav bar on scroll
